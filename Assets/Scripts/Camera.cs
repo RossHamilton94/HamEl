79126623481e8f6758a0d173fd77416dfc51e65b
UselessBanana <uselessbananalikespotatoes@gmail.com>
2016-11-04 17:02:29 +0000

    Updating repo

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Rigidbody player = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+
+        transform.LookAt(player.transform);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
-    public float speed = 1.0f;
+    public float speed = 12.0f;
 
     private Rigidbody rb;
 
@@ -20,5 +20,30 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
-	}
+
+        RotateByMovement();
+    }
+
+    void RotateByMovement()
+    {
+        float hSpeed = rb.velocity.x;
+        float vSpeed = rb.velocity.y;
+
+        if (hSpeed < 0.005f)
+        {
+            hSpeed = 0.0f;
+        }
+
+        if (vSpeed < 0.005f)
+        {
+            vSpeed = 0.0f;
+        }
+
+        float angleOfTravel = Mathf.Atan(hSpeed / vSpeed);
+        angleOfTravel = angleOfTravel * Mathf.Rad2Deg;
+
+        //transform.rotation = new Vector3(0.0f, angleOfTravel, 0.0f)
+
+        transform.localEulerAngles = new Vector3(0.0f, angleOfTravel, 0.0f);
+    }
 }
